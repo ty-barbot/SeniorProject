90f0ffd9741ef51f0c8fe41ec309c3d895f05d4f
Makenna <makenna.peters@und.edu>
2021-03-03 17:06:51 -0600

    changes for planning workout page

--- undworkoutapp/main_app/static/js/plan.js
+++ undworkoutapp/main_app/static/js/plan.js
@@ -1,15 +1,24 @@
 jQuery(document).ready(function () {
     var $template = $(".template");
-    var hash = 2;
+    var hash = 1;
     jQuery(".btn-add-panel").on("click", function () {
         var $newPanel = $template.clone();
-        $newPanel.find(".collapse").removeClass("in");
-        $newPanel.find(".toggle").attr("data-target", "#collapse" + (++hash))
-            .text("Workout #" + hash);
-        $newPanel.find(".collapse").attr("id", "collapse" + hash).attr("aria-labelledby", "heading" + hash);
-        $newPanel.find(".card-header").attr("id", "heading" + hash);
-        jQuery("#accordionExample").append($newPanel.fadeIn());
+        $newPanel.find(".panel-heading").attr("id", "#plan-heading-" + (++hash));
+        $newPanel.find(".panel-collapse").attr("id", "plan-collapse-" + hash).attr("aria-labelledby", "plan-heading-" + hash);
+        $newPanel.find(".plan-heading-link").attr("href", "#plan-collapse-" + hash).attr("aria-controls", "plan-collapse-" + hash).text("Exercise #" + hash);
+        $newPanel.find(".panel-set-info").attr("id", "#plan-collapse-" + hash);
+        $newPanel.find(".btn-add-set").attr("id", "plan-collapse-" + hash);
+        $newPanel.find(".panel-set-info").empty();
+        jQuery("#accordion").append($newPanel.fadeIn());
     });
+    var $setForm = $(".set-form");
+    jQuery("body").on("click", ".btn-add-set", function (e) {
+        var idClicked = e.target.id;
+        var $newsetForm = $setForm.clone();
+        jQuery("#"+idClicked + " .panel-set-info").append($newsetForm.fadeIn());
+    });
+
+
     jQuery('.spinner-border').hide();
     console.log("here");
 });
